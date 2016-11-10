6807f03f64f4cf9200204d68a24c07e0d8fe6436
Wojciech Charuza <wojciechcharuza@iMac.local>
2016-11-10 22:50:29 +0100

    Email auth added

--- SocialApp/SignInVC.swift
+++ SocialApp/SignInVC.swift
@@ -13,9 +13,14 @@ import Firebase
 
 class SignInVC: UIViewController {
 
+    @IBOutlet weak var emailField: CustomTextField!
+    @IBOutlet weak var passwordField: CustomTextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         
     }
 
@@ -36,6 +41,29 @@ class SignInVC: UIViewController {
                 let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
                 self.firebaseAuth(credential)
             }
+        }
+    }
+    
+    @IBAction func signInTapped(_ sender: AnyObject) {
+        
+        if let email = emailField.text , let pwd = passwordField.text {
+            
+            FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                
+                if error == nil {
+                    print("WOJTEK: EMAIL USER AUTHENTICATED WITH FIREBASE")
+                } else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil {
+                            
+                        print("WOJTEK: UNABLE TO AUTHENTICATE WITH FIREBASE - \(error)")
+                        } else {
+                            
+                            print("WOJTEK: SUCCESFULLY AUTHENTICATED WITH FIREBASE")
+                        }
+                    })
+                }
+            })
         }
     }
     
