fa08124df0f9b45385fbdf8f2c2fcb3269aaa2fd
Wojciech Charuza <wojciechcharuza@iMac.local>
2016-11-10 15:16:25 +0100

    first commit, firebase setup

---
+++ SocialApp/SignInVC.swift
@@ -0,0 +1,23 @@
+//
+//  SignInVC.swift
+//  SocialApp
+//
+//  Created by Wojciech Charuza on 10.11.2016.
+//  Copyright © 2016 Wojciech Charuza. All rights reserved.
+//
+
+import UIKit
+
+class SignInVc: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+
+
+}
+
