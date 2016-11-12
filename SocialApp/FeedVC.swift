4499d967c40e3ebc6e9702ce3f961bfbd52f419f
Wojciech Charuza <wojciechcharuza@iMac.local>
2016-11-12 19:08:02 +0100

    completed getting firebase data

--- SocialApp/FeedVC.swift
+++ SocialApp/FeedVC.swift
@@ -20,10 +20,12 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.delegate = self
         tableView.dataSource = self
         
-        
-        
-
-        
+        DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
+            
+            print(snapshot.value)
+            
+        })
+ 
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
